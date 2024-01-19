224a0b5611b9d31121b717a26b4d3ea31bd568c6
f4s4r <sesetimokhin@edu.hse.ru>
2024-01-19 23:00:40 +0300

    first commit

---
+++ MyPriorityQueue.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 4s4r on 19.01.2024.
+//
+
+#include "MyPriorityQueue.h"
+template <class T>
+
+MyPriorityQueue<T>::MyPriorityQueue() :
+    queue_(NULL), size_(0)
+{}
---
+++ MyPriorityQueue.h
@@ -0,0 +1,21 @@
+//
+// Created by 4s4r on 19.01.2024.
+//
+#include <iostream>
+#ifndef CPP_LAB_10_MYPRIORITYQUEUE_H
+#define CPP_LAB_10_MYPRIORITYQUEUE_H
+
+template <class T>
+class MyPriorityQueue {
+private:
+    size_t size_;
+    T* queue_;
+
+public:
+    //parameterless constructor
+    MyPriorityQueue();
+
+};
+
+
+#endif //CPP_LAB_10_MYPRIORITYQUEUE_H
